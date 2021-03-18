91de3ee95427f8943e8d9412e83be8e0d14093c7
lerrry <550500499@qq.com>
2021-03-19 00:23:21 +0530

    新建一个hellojava文件 Signed-off-by: lerry <550500499@qq.com>

---
+++ HelloJava.java
@@ -0,0 +1,56 @@
+package com.lerry.demo;
+
+//public class HelloJava {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		System.out.println("Hello Java");
+//		
+//
+//	}
+//
+//}
+
+
+public class HelloJava {
+    /*第一个java程序
+	 *它将打印字符串 Hello Java
+	 */
+	public static void main(String[] args){
+		System.out.println("HelloJava123"); // 打印HellJava
+		
+//		int a = 3;
+//		int b = 2;
+//		int c = 5;
+//		int d = 10;
+//		System.out.println(a+b);
+//		System.out.println(a+b/d);
+//		System.out.println((a+b)/c*d);
+////		
+		int x=1, y=4;
+		int z;
+		z=x+y;
+		
+//		System.out.println(x ==1 || y ==3);
+//		System.out.println(!(x ==1) || y ==3);
+//		System.out.println(x ==1 && y ==3);
+//		System.out.println(x ==1 && y ==4 ||z==3);
+//		
+//		
+//		
+//		
+//		boolean result = z!=5;
+//		System.out.println(result);
+		z = x++ << 1 * ++y  >> 1 / y++ << 1 + y >> x - 1 >> 1;
+		System.out.println(z);
+		
+//		String str1="lerry";
+////		String str2="lerry";
+//		String str2=new String("lerry");
+//		System.out.println(str1.equals(str2));
+//		
+
+	}
+}
+
+		 
